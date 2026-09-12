df83d8af9f3dd3d59710405ba477fdb9106d6b71
fortydeea <40deea@gmail.com>
2026-09-11 17:55:17 -0700

    Move python lsp config into init.lua

--- init.lua
+++ init.lua
@@ -699,7 +699,14 @@ require('lazy').setup({
       local servers = {
         clangd = {},
         -- gopls = {},
-        pyright = {},
+        basedpyright = {
+          settings = {
+            basedpyright = {
+              analysis = { typeCheckingMode = 'basic' },
+            },
+          },
+        },
+        ruff = {},
         -- rust_analyzer = {},
         -- ... etc. See `:help lspconfig-all` for a list of all the pre-configured LSPs
         --
